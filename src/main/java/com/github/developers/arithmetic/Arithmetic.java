fab16d508170d96f8a5629449b6d630cacb04bdf
katherine <katherine@dalt.cl>
2021-11-22 17:35:27 -0300

    first commit

---
+++ src/main/java/com/github/developers/arithmetic/Arithmetic.java
@@ -0,0 +1,10 @@
+package com.github.developers.arithmetic;
+
+import java.math.BigDecimal;
+
+public interface Arithmetic {
+
+	public BigDecimal add (BigDecimal x, BigDecimal y);
+	public BigDecimal substract (BigDecimal x, BigDecimal y);
+	
+}
---
+++ src/main/java/com/github/developers/arithmetic/ArithmeticImpl.java
@@ -0,0 +1,17 @@
+package com.github.developers.arithmetic;
+
+import java.math.BigDecimal;
+
+public class ArithmeticImpl implements Arithmetic {
+
+	@Override
+	public BigDecimal add(BigDecimal x, BigDecimal y) {
+		return x.add(y);
+	}
+
+	@Override
+	public BigDecimal substract(BigDecimal x, BigDecimal y) {
+		return x.subtract(y);
+	}
+
+}
